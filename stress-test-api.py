b13c76bfe0dede68ea13d74964828b5e9e2574fc
master <netort@internet.ru>
2025-03-01 01:40:30 +0400

    Stress-test

--- stress-test-api.py
+++ stress-test-api.py
@@ -1,25 +1,25 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, constant
 from threading import Lock
 from logging import getLogger
 import random
-from multiprocessing import Value
+from decimal import Decimal
 
 class TestWallet(HttpUser):
-    wait_time = between(0.1, 0.5)
+    wait_time = constant(0)
     balance_lock = Lock()
     logger = getLogger(__name__)
     wallet_uuid = None
-    balance = Value("d", 1000.0)
+    balance = Decimal('100.00')
 
     def on_start(self):
-        if TestWallet.wallet_uuid is None:
-            self.create_wallet()
+        with TestWallet.balance_lock:
+            if TestWallet.wallet_uuid is None:
+                self.create_wallet()
 
     def create_wallet(self):
-        response = self.client.post(f"/api/v1/wallets/new_wallet/{round(TestWallet.balance.value, 2)}", catch_response=True)
+        response = self.client.post(f"/api/v1/wallets/new_wallet/{TestWallet.balance}")
         if response.status_code == 200:
-            with TestWallet.balance_lock:
-                TestWallet.wallet_uuid = response.json().get("wallet_uuid")
+            TestWallet.wallet_uuid = response.json().get("wallet_uuid")
             TestWallet.logger.info(f"Wallet created, uuid: {TestWallet.wallet_uuid}")
         else:
             raise Exception(f"Failed to create wallet: {response.status_code}")
@@ -34,7 +34,7 @@ class TestWallet(HttpUser):
 
     @task
     def withdraw(self):
-        amount = round(random.uniform(0.1, 100), 2)
+        amount = Decimal(random.uniform(0.1, 100))
         with self.client.post(f"/api/v1/wallets/{TestWallet.wallet_uuid}/operation", json={
             "operation": "WITHDRAW",
             "amount": amount
@@ -49,7 +49,7 @@ class TestWallet(HttpUser):
 
     @task
     def deposit(self):
-        amount = round(random.uniform(0.1, 100), 2)
+        amount = Decimal(random.uniform(0.1, 100))
         with self.client.post(f"/api/v1/wallets/{TestWallet.wallet_uuid}/operation", json={
             "operation": "DEPOSIT",
             "amount": amount
